8ab462f286530e7d74e148bf7d8cc59ccfb89467
satoshiogawa <septemberseventeen1984@gmail.com>
2016-09-03 13:45:36 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
@@ -17,11 +19,33 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はリダイレクト
+      redirect_to @user , notice: '基本情報を更新しました'
+    else
+      # 保存に失敗した場合はedit画面へ戻す
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :age, :nickname, :area)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
