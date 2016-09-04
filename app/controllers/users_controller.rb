ef1e3823a3a88f1181b91d793ef509128b442b66
satoshiogawa <septemberseventeen1984@gmail.com>
2016-09-04 14:38:50 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
@@ -42,10 +41,5 @@ class UsersController < ApplicationController
   
   def set_user
     @user = User.find(params[:id])
-  end
-  
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  
   get    'signup', to: 'users#new'
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get    'edit' , to: 'users#edit'
-  post   'edit' , to: 'users#updat'
-
   resources :users
 end
